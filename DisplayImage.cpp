e5fe2611e863697ab68ba9aac544d78cf200332b
Ashok Gowtham M <ashokgo@thoughtworks.com>
2014-09-12 19:04:38 +0530

    Ashok | neon effect using diff of two gaussian blurs

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -23,40 +23,68 @@ double avg(std::queue<double> q) {
 
 int main(int argc, char** argv )
 {
-    // if ( argc != 2 )
-    // {
-    //     printf("usage: DisplayImage.out <Image_Path>\n");
-    //     return -1;
-    // }
+    std::queue<double> q;
+    double b=1000;
+    CvCapture* capture = cvCreateCameraCapture(-1);  //Capture using any camera connected to your system
 
     namedWindow("Display Image", WINDOW_AUTOSIZE );
-    // imshow("Display Image", image);
-    std::queue<double> q;
-    CvCapture* capture = cvCreateCameraCapture(-1);  //Capture using any camera connected to your system
+
     while(1){ //Create infinte loop for live streaming
         clock_t begin, end;
         double time_spent;
         begin = clock();
 
-
         IplImage* frame = cvQueryFrame(capture); //Create image frames from capture
         Mat image = cvarrToMat(frame);
 
-        Mat gray_image;
-        cvtColor( image, gray_image, CV_BGR2GRAY );
-        if(!gray_image.data) {
-            printf("gray_image empty\n");
+        Mat blurred1;
+        Mat blurred2;
+        Mat diff;
+        Mat normalized;
+        Size ksize;
+        ksize.height=ksize.width=5;
+        GaussianBlur( image, blurred1, ksize, 0 );
+        if(!blurred1.data) {
+            printf("blurred1 empty\n");
+            exit(-1);
+        }
+        ksize.height=ksize.width=15;
+        GaussianBlur( image, blurred2, ksize, 0 );
+        if(!blurred2.data) {
+            printf("blurred2 empty\n");
+            exit(-1);
+        }
+        absdiff(blurred1, blurred2, diff );
+        if(!diff.data) {
+            printf("diff empty\n");
             exit(-1);
         }
 
-        imshow("Display Image", gray_image);
-        // cvShowImage("Display Image", image);
+        normalize(diff, normalized, 0, (int)b, NORM_MINMAX);
+        if(!normalized.data) {
+            printf("normalized empty\n");
+            exit(-1);
+        }
+
+        imshow("Display Image", normalized);
+        // cvShowImage("Display Image", frame);
 
         char key = waitKey(10);     //Capture Keyboard stroke
         if (char(key) == 27){
             break;      //If you hit ESC key loop will break.
         }
-
+        switch(char(key)) {
+        case 45:
+            b*=2;
+            printf("Brightness Increased: %lf\n", b);
+            break;
+        case 61:
+            b/=2;
+            printf("Brightness Increased: %lf\n", b);
+            break;
+        default:
+            printf("key press: %d\n", key);
+        }
 
         end = clock();
         time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
@@ -65,22 +93,6 @@ int main(int argc, char** argv )
         printf("%3.1lf fps.  %3.5lf sec. avg:%3.1lf\n",1/time_spent, time_spent, 1/avg(q));
     }
     cvReleaseCapture(&capture); //Release capture.
-
-    // waitKey(0);
-
-    // cvNamedWindow("Camera_Output", 1);    //Create window
-    // CvCapture* capture = cvCaptureFromCAM(CV_CAP_ANY);  //Capture using any camera connected to your system
-    // while(1){ //Create infinte loop for live streaming
- 
-    //     IplImage* frame = cvQueryFrame(capture); //Create image frames from capture
-    //     cvShowImage("Camera_Output", frame);   //Show image frames on created window
-    //     key = cvWaitKey(10);     //Capture Keyboard stroke
-    //     if (char(key) == 27){
-    //         break;      //If you hit ESC key loop will break.
-    //     }
-    // }
-    // cvReleaseCapture(&capture); //Release capture.
-    // cvDestroyWindow("Camera_Output"); //Destroy Window
 
 
     return 0;
